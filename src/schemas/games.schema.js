001b32e718cbcb630d1edb437faf22adb6059093
Gabriel Chaves <gbrielchaves9@gmail.com>
2023-05-15 16:03:06 -0300

    erro cliente voltou

--- src/schemas/games.schema.js
+++ src/schemas/games.schema.js
@@ -3,6 +3,6 @@ import joi from "joi";
 export const createSchemaGame = joi.object({
     name: joi.string().min(2).required(),
     image: joi.string().uri().required(),
-    stockTotal: joi.number().integer().min(0).required(),
+    stockTotal: joi.number().integer().min(1).required(),
     pricePerDay: joi.number().positive().required()
 });
