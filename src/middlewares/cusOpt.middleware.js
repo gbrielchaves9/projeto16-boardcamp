f767378a93acdc48819f179ab5e7e114a2df7b6c
Gabriel Chaves <gbrielchaves9@gmail.com>
2023-05-15 16:13:21 -0300

    cliente quase ok

--- src/middlewares/cusOpt.middleware.js
+++ src/middlewares/cusOpt.middleware.js
@@ -2,18 +2,27 @@ import { createSchemaCust } from "../schemas/customers.schema.js";
 import { db } from "../configs/database.js";
 
 export async function validSchemaCust(req, res, next) {
-  const customer = req.body;
-  const { error } = createSchemaCust.validate(customer);
-  if (error) {
-    const errors = error.details.map((details) => details.message);
-    return res.status(400).send({ errors });
-  }
+  const { name, phone, cpf, birthday } = req.body;
 
-  const existingCustomer = await db.query('SELECT * FROM customers WHERE cpf = $1', [customer.cpf]);
-  if (existingCustomer.rows.length > 0) {
-    return res.status(409).send({ error: "O CPF já está cadastrado no banco de dados." });
+  try {
+ 
+    const existingCustomer = await db.query('SELECT * FROM customers WHERE cpf = $1', [cpf]);
+    if (existingCustomer.rows.length > 0) {
+      return res.status(409).send({ error: 'O cliente já existe no banco de dados.' });
+    }
+
+
+    await db.query(`
+      INSERT INTO customers (name, phone, cpf, birthday)
+      VALUES ($1, $2, $3, $4);
+    `, [name, phone, cpf, birthday]);
+
+    res.status(201).send('Cliente criado com sucesso.');
+  } catch (error) {
+    res.status(500).send(error.message);
   }
 
   res.locals.customer = customer;
   next();
 }
+
