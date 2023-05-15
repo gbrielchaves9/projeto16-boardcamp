fc0b9d240d749a8b9c4917ff730bb65af18a2f0a
Gabriel Chaves <gbrielchaves9@gmail.com>
2023-05-15 14:06:33 -0300

    erro 1 concertadti

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,15 +1,16 @@
 import { db } from "../configs/database.js";
 
 export async function create(req, res) {
-    const { name, image, stockTotal, pricePerDay, } = res.locals.game
+    const { name, image, stockTotal, pricePerDay } = res.locals.game
+
     try {
         await db.query(`
-        insert into games(name, image, 'stockTotal', 'pricePerDay')
-        VALUES($1, $2, $3, $4)`,
-        [name, image, stockTotal, pricePerDay]
-        )
-        res.status(201)
+      INSERT INTO games (name, image, "stockTotal", "pricePerDay") 
+      VALUES ($1, $2, $3, $4);
+      `, [name, image, stockTotal, pricePerDay])
+
+        res.sendStatus(201)
     } catch (error) {
-        res.status(500).send(error.message)
+        res.status(500).send(error.message);
     }
 }
